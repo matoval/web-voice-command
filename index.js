5d062e68bfeb0f23a28b51121cd47047473d6910
Matthew Sandoval <mat24c@gmail.com>
2020-05-28 13:00:55 -0700

    add coming soon in readME, change function name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.start = () => {
+module.exports.webVoiceCommand = () => {
   window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition
   const listening = new SpeechRecognition()
   listening.start()
