4be39a7d3de658b8abe5271f336adb52bc0c5eae
Matthew Sandoval <mat24c@gmail.com>
2020-06-14 23:24:46 -0700

    added corti to mock the web speech api

--- index.test.js
+++ index.test.js
@@ -1,7 +1,29 @@
-import webVoiceCommand from 'web-voice-command'
+const webVoiceCommand = require('web-voice-command')
+const { Corti } = require('./node_modules/corti/src/corti')
 
-describe('setOptions', () => {
-  it('does setOptions exist', () => {
-    expect()
-  })
-})
+describe('Corti.patch', function() {
+
+  it('is Corti an object', function () {
+    expect(typeof Corti).toBe('object');
+  });
+
+  it('is Corti.patch a function', function () {
+    expect(typeof Corti.patch).toBe('function');
+  });
+
+  it('is Corti.unpatch a function', function () {
+    expect(typeof Corti.unpatch).toBe('function');
+  });
+
+  it('is Corti defined should fail', function () {
+    expect(Corti.patch).toBeDefined();
+  });
+
+  it('should make SpeechRecognition defined', function () {
+    expect(window.SpeechRecognition).toBeUndefined();
+    Corti.patch();
+    expect(window.SpeechRecognition).toBeDefined();
+    Corti.unpatch();
+  });
+
+});
