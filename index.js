dd1add8cba658e424fcc552a6a95133d2cd0991d
Matthew Sandoval <mat24c@gmail.com>
2020-06-15 11:54:25 -0700

    testing pre-commit hooks

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
-let listening
-let listeningToggle = false
+let listening;
+let listeningToggle = false;
 //Set options
 function setOptions(newOptions) {
   // const window = window
 
   //Call browser API for Web Speech
   window.SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition
-  listening = new SpeechRecognition()
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  listening = new SpeechRecognition();
 
   //Options defaults
   let options = {
     persistentListening: false,
     listenOnLoad: false,
     language: "en-US",
-  }
+  };
 
-  options = { ...options, ...newOptions }
+  options = { ...options, ...newOptions };
 
   if (options.persistentListening) {
-    listening.start()
-    listening.addEventListener('end', listening.start)
+    listening.start();
+    listening.addEventListener("end", listening.start);
   }
 
   if (options.listenOnLoad) {
-    listening.start()
+    listening.start();
   }
 
   if (options.language) {
-    listening.lang = options.language
+    listening.lang = options.language;
   }
 }
 
 //Get all voice inputs
 function results(callback) {
   listening.onresult = function (e) {
-    const results = e.results[0]
-    const transcripts = results[0].transcript.split(' ')
-    return callback(transcripts)
-  }
+    const results = e.results[0];
+    const transcripts = results[0].transcript.split(" ");
+    return callback(transcripts);
+  };
 }
 
 //Start and stop listening using one or two buttons
 function startStop(numberOfButtons, button) {
   if (numberOfButtons === 1) {
-    listeningToggle = !listeningToggle
+    listeningToggle = !listeningToggle;
     if (listeningOn) {
-      listening.start()
-      listening.addEventListener('end', listening.start)
+      listening.start();
+      listening.addEventListener("end", listening.start);
     } else {
-      listening.removeEventListener('end', listening.start)
-      listening.stop()
+      listening.removeEventListener("end", listening.start);
+      listening.stop();
     }
   } else if (numberOfButtons === 2) {
-    const buttonClicked = button
-    if (buttonClicked === 'start') {
-      listening.start()
-      listening.addEventListener('end', listening.start)
-    } else if (buttonClicked === 'stop') {
-      listening.removeEventListener('end', listening.start)
-      listening.stop()
+    const buttonClicked = button;
+    if (buttonClicked === "start") {
+      listening.start();
+      listening.addEventListener("end", listening.start);
+    } else if (buttonClicked === "stop") {
+      listening.removeEventListener("end", listening.start);
+      listening.stop();
     }
   } else {
     console.log(
       `Error startStopTrigger was passed more than two numberOfButtons`
-    )
+    );
   }
 }
 //Get results that match word list
 function wordList(words, withActions, callback) {
-  let actions = []
+  let actions = [];
   listening.onresult = function (e) {
-    const results = e.results[0]
-    const transcripts = results[0].transcript.split(' ')
+    const results = e.results[0];
+    const transcripts = results[0].transcript.split(" ");
     if (transcripts.length > 0) {
       //return a single word said after the actionWord
       if (withActions) {
-        actions = words
+        actions = words;
         for (const action of actions) {
           if (action.actionWord.name === transcripts[0]) {
-            actions = action.actionWord
+            actions = action.actionWord;
             for (const word of actions.options) {
               if (word.word === transcripts[1]) {
-                return callback(word.word)
+                return callback(word.word);
               }
             }
           }
@@ -90,17 +90,17 @@ function wordList(words, withActions, callback) {
       }
       //returns an array of all words that match the list of words
       else {
-        let filteredTranscript = []
+        let filteredTranscript = [];
         for (const word of words) {
           filteredTranscript = [
             ...filteredTranscript,
             ...transcripts.filter((match) => match === word.word.name),
-          ]
+          ];
         }
-        return callback(filteredTranscript)
+        return callback(filteredTranscript);
       }
     }
-  }
+  };
 }
 
-module.exports = { setOptions, results, startStop, wordList }
+module.exports = { setOptions, results, startStop, wordList };
