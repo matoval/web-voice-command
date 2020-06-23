c1a0b8cb4980d30fe539bb26589d26855c21daab
Matthew Sandoval <mat24c@gmail.com>
2020-06-23 09:32:42 -0700

    adding testing

--- index.test.js
+++ index.test.js
@@ -1,5 +1,4 @@
 const webVoiceCommand = require('web-voice-command')
-const { Corti } = require('./node_modules/corti/src/corti')
 
 describe('webVoiceCommand and methods defined', function () {
   it('wordList is a function', function () {
@@ -20,5 +19,32 @@ describe('webVoiceCommand and methods defined', function () {
 
   it('wordList is a function', function () {
     expect(typeof webVoiceCommand.wordList).toBe('function')
+  })
+})
+
+describe('Mock the SpeechRecognition object', function () {
+  // SpeechRecognition = jest.fn()
+  const mockSpeech = {
+    results() {
+      return (event = {
+        results: [{ transcript: 'test test test' }],
+      })
+    },
+  }
+  beforeEach(() => {
+    const listening = jest.spyOn(mockSpeech, 'window.SpeechRecognition')
+  })
+
+  it('SpeechRecognition is defined', function () {
+    expect(window.SpeechRecognition).toBeDefined()
+  })
+
+  it('setOptions calls start function', function () {
+    expect(webVoiceCommand.setOptions).toBeDefined()
+  })
+
+  it('setOptions calls SpeechRecognition function', function () {
+    webVoiceCommand.results()
+    expect(window.SpeechRecognition).toHaveBeenCalled()
   })
 })
